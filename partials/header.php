719adcf27f7c9c1c979537032bd817748f14983a
Donatella7 <campaniellodonatella@gmail.com>
2022-04-22 22:49:26 +0200

    style header select

--- partials/header.php
+++ partials/header.php
@@ -4,16 +4,14 @@
             <div class="col-2">
                 <img class="header-logo" src="css/img/logo-small.png" alt="logo">
             </div>
-            <div class="col-4">
-                <select @change="changeGenere()" v-model="selectGenere" class="form-select" name="" id="">
+            <div class="col-4 d-flex justify-content-end">
+                <select @change="changeGenere()" v-model="selectGenere" class="my-select" name="" id="form">
                     <option class="text-white bg-dark" value=""> All Genres </option>
                     <option class="text-white bg-dark" v-for = "genere in  generiDischi()" :value="genere">
                         {{genere}}
                     </option>
                 </select>
             </div>
-
-            <p>{{selectGenere}}</p>
         </div>
     </section>
 </header>
